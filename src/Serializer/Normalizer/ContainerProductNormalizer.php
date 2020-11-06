1df84f165b57ead503064025dc4a6e574e9e9b77
remi <remi.guerin.1@etu.univ-amu.fr>
2020-11-06 14:32:01 +0100

     

---
+++ src/Serializer/Normalizer/ContainerProductNormalizer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class ContainerProductNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+{
+    private $normalizer;
+
+    public function __construct(ObjectNormalizer $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    public function normalize($object, string $format = null, array $context = []): array
+    {
+        $data = [
+            'id' => $object->getId(),
+            'container_id' => $object->getContainerId(),
+            'product_id' => $object->getProductId(),
+            'quantity_id' => $object->getQuantityId(),
+        ];
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return $data instanceof \App\Entity\ContainerProduct;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+}
