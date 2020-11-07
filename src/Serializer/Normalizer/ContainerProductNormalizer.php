82ed9809bfe47d891a5565005063e09b84ad21c0
DuvalQuentin <DuvalQuentin83@gmail.com>
2020-11-07 16:09:44 +0100

    ajout d'autres conteneurs

---
+++ src/Serializer/Normalizer/ContainerProductNormalizer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class ContainerProductNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+{
+    public function normalize($object, string $format = null, array $context = []): array
+    {
+        return [
+            'id' => $object->getId(),
+            'container_id' => $object->getContainerId(),
+            'product_id' => $object->getProductId(),
+            'quantity' => $object->getQuantity()
+        ];
+    }
+
+    public function supportsNormalization($data, string $format = null): bool
+    {
+        return $data instanceof \App\Entity\ContainerProduct;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+}
