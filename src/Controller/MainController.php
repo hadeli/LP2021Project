bab2a14a00eb3c9d049778091f25ca0e45718827
ALEXIS <alexis.vatin@etu.univ-amu.fr>
2020-11-11 16:52:08 +0100

    Rendu Projet LP

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class MainController extends AbstractController
 {
@@ -18,7 +19,8 @@ class MainController extends AbstractController
     /**
      * @Route("/container", name="app_container")
      */
-    public function containerList():Response{
+    public function containerList(): Response
+    {
         $manager = $this->getDoctrine()->getManager();
         $containerList = $manager->getRepository(Container::class)->findAll();
         return $this->json($containerList);
@@ -28,22 +30,23 @@ class MainController extends AbstractController
      * @Route("/container/{id}", name="app_container_id")
      */
 
-    public function UniqueContainer($id, Request $request):Response{
-        switch ($id){
+    public function UniqueContainer($id, Request $request): Response
+    {
+        switch ($id) {
             case 'new' :
                 $manager = $this->getDoctrine()->getManager();
                 if (!$manager->getRepository(ContainerModel::class)->findOneBy(['id' => $request->query->getInt('model')]))
-                    return new Response('Aucun model avec cet id : ' .$id);
+                    return new Response('Aucun model avec cet id : ' . $id);
                 if (!$manager->getRepository(ContainerShip::class)->findOneBy(['id' => $request->query->getInt('ship')]))
-                    return new Response('Aucun ship avec cet id : ' .$id);
+                    return new Response('Aucun ship avec cet id : ' . $id);
 
                 $ship = $manager->getRepository(ContainerShip::class)->findOneBy(['id' => $request->query->getInt('ship')]);
-                $nb=0;
+                $nb = 0;
                 $nombre = $manager->getRepository(Container::class)->findBy(['containership_id' => $request->query->getInt('ship')]);
-                foreach ($nombre as $key){
+                foreach ($nombre as $key) {
                     $nb++;
                 }
-                if($ship->getContainerLimit() > $nb+1){
+                if ($ship->getContainerLimit() > $nb + 1) {
                     $container = new Container();
                     $container->setColor($request->query->get('color'));
                     $container->setContainerModelId($request->query->get('model'));
@@ -52,11 +55,11 @@ class MainController extends AbstractController
                     $manager->persist($container);
                     $manager->flush();
                     return new Response('Container ajouté');
-                }else{
-                    return new Response('Le ship ' .$ship->getName(). 'contient trop de container');
+                } else {
+                    return new Response('Le ship ' . $ship->getName() . 'contient trop de container');
                 }
             default:
-                if($id < 3){
+                if ($id < 3) {
                     return $this->json(['aucune valeur']);
                 }
                 $manager = $this->getDoctrine()->getManager();
@@ -67,7 +70,8 @@ class MainController extends AbstractController
     /**
      * @Route("/containership", name="app_containership")
      */
-    public function containershipList():Response{
+    public function containershipList(): Response
+    {
         $manager = $this->getDoctrine()->getManager();
         $containershipList = $manager->getRepository(ContainerShip::class)->findAll();
         return $this->json($containershipList);
@@ -76,8 +80,9 @@ class MainController extends AbstractController
     /**
      * @Route("/containership/{id}", name="containership_id")
      */
-    public function Uniquecontainership($id, Request $request):Response{
-        switch ($id){
+    public function Uniquecontainership($id, Request $request): Response
+    {
+        switch ($id) {
             case 'new':
                 $shipmanager = $this->getDoctrine()->getManager();
                 $ship = new ContainerShip();
@@ -89,162 +94,77 @@ class MainController extends AbstractController
 
                 return new Response('Container créé');
 
+            default :
+                if ($id < 3) {
+                    return $this->json(['Aucune valeur']);
+                }
 
-        }
-    }
-
-
-    public function containershipId($id):Response{
-        if ($id == "new") {
-            $request = Request::createFromGlobals();
-            if ($request->getMethod() == "POST") {
                 $manager = $this->getDoctrine()->getManager();
+                $ship = $manager->getRepository(ContainerShip::class)->findOneBy(['id' => $id]);
 
-                $container = new ContainerShip();
-
-                if (isset($_POST['name'])) {
-                    $container->setName($_POST['name']);
-                }
-
-                if (isset($_POST['captainName'])) {
-                    $container->setCaptainName()($_POST['captainName']);
-                }
-
-                if (isset($_POST['containerLimit'])) {
-                    $container->setContainerLimit($_POST['containerLimit']);
-                }
-
-                $manager->persist($container);
-                $manager->flush();
-
-//                return $this->json($container);
-                return new Response("containership ajouté");
-            }
-        }else {
-            $manager = $this->getDoctrine()->getManager();
-            $containershipId = $manager->getRepository(ContainerShip::class)->findOneBy(['id' => $id]);
-
-            if ($containershipId == NULL) {
-                $containershipId = [
-                    'error' => 'Aucun containership avec id = ' . $id
-                ];
-            }
-
-            return $this->json($containershipId);
+                return $this->json(ship);
         }
     }
 
     /**
-     * @Route("/product", name="product")
+     * @Route("/product", name="app_product")
      */
-    public function productList():Response{
-        $manager = $this->getDoctrine()->getManager();
-        $productList = $manager->getRepository(Product::class)->findAll();
-        return $this->json($productList);
-    }
-
-    /**
-     * @Route("/product/{id}", name="productId")
-     */
-    public function productId($id):Response
+    public function productList(): Response
     {
-        if ($id == "new") {
-            $request = Request::createFromGlobals();
-            if ($request->getMethod() == "POST") {
+        $manager = $this->getDoctrine()->getManager();
+        $productList = $manager->getRepository(ContainerShip::class)->findAll();
+        return $this->json($productList);
 
-                $manager = $this->getDoctrine()->getManager();
-
-                $container = new Product();
-
-                if (isset($_POST['name'])) {
-                    $container->setName($_POST['name']);
-                }
-
-                if (isset($_POST['length'])) {
-                    $container->setLength($_POST['length']);
-                }
-
-                if (isset($_POST['width'])) {
-                    $container->setWidth($_POST['width']);
-                }
-
-                if (isset($_POST['height'])) {
-                    $container->setHeight($_POST['height']);
-                }
-
-
-                $manager->persist($container);
-                $manager->flush();
-
-//                return $this->json($container);
-                return new Response("produit ajouté");
-            }
-        } else {
-            $manager = $this->getDoctrine()->getManager();
-            $productId = $manager->getRepository(Product::class)->findOneBy(['id' => $id]);
-
-            if ($productId == NULL) {
-                $productId = [
-                    'error' => 'Aucun produit avec id = ' . $id
-                ];
-            }
-            return $this->json($productId);
-        }
     }
-
-
-
-//    /**
-//     * @Route("/container/new", name="app_container_new", methods={"POST"})
-//     */
-//    public function newContainerShow(): Response{
-//
-//     }
-
-//    /**
-//     * @Route("/product/new", name="app_product_new", methods={"POST"})
-//     */
-//    public function newProductShow(): Response{
-//
-//    }
-
 
     /**
-     * @Route("/containerproduct/new", name="app_containerproduct_new", methods={"POST"})
+     * @Route("/product/{id}", name="app_product_id")
      */
-    public function newContainerproductShow(): Response{
-        $request = Request::createFromGlobals();
-        if ($request->getMethod() == "POST") {
-            $manager = $this->getDoctrine()->getManager();
+    public function Uniqueproduct($id, Request $request): Response
+    {
+        switch ($id) {
+            case 'new':
+                $manager = $this->getDoctrine()->getManager();
 
-            $container = new ContainerProduct();
+                $product = new Product();
+                $product->setName($request->query->get('name'));
+                $product->setHeight($request->query->get('height'));
+                $product->setLength($request->query->get('length'));
+                $product->setWidth($request->query->get('width'));
 
-            if (isset($_POST['containerId'])) {
-                $container->setContainerId($_POST['containerId']);
-            }
+                $manager->persist($product);
+                $manager->flush();
 
-            if (isset($_POST['productId'])) {
-                $container->setProductId($_POST['productId']);
-            }
+                return new Response('Produit créé');
 
-            if (isset($_POST['quantity'])) {
-                $container->setQuantity($_POST['quantity']);
-            }
+            default :
+                $manager = $this->getDoctrine()->getManager();
+                $product = $manager->getRepository(Product::class)->findOneBy(['id' => $id]);
 
-            $manager->persist($container);
-            $manager->flush();
-
-            return $this->json($container);
+                return $this->json($product);
         }
     }
 
-//    /**
-//     * @Route("/containership/new", name="app_containerproduct_new", methods={"POST"})
-//     */
-//    public function newContainership(): Response{
-//
-//    }
+    /**
+     * @Route("/product-container/new", name="app_containerproduct_new")
+     */
+    public function addProduct(Request $request): Response{
+
+        $manager = $this->getDoctrine()->getManager();
+
+        if(!$manager->getRepository(Container::class)->findOneBy(['id' => $request->query->getInt('container')]))
+            return new Response('Pas de conainter');
+
+        if(!$manager->getRepository(Product::class)->findOneBy(['id' => $request->query->getInt('product')]))
+            return new Response('Pas de produit');
+
+
+
 
 
 }
 
+
+
+
+}
