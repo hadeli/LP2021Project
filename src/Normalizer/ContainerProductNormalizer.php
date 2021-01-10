723e52f248716528ed66df5ee8abf70262a00fc5
NAVARRO Anthony <anthony.navarro@etu.univ-amu.fr>
2021-01-10 15:47:38 +0100

    Création des Normalizer

---
+++ src/Normalizer/ContainerProductNormalizer.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Normalizer;
+
+use App\Entity\ContainerProduct;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+
+class ContainerProductNormalizer  implements ContextAwareNormalizerInterface
+{
+    public function supportsNormalization($data, string $format = null, array $context = [])
+    {
+        return $data instanceof ContainerProduct;
+    }
+
+    /**
+     * @param ContainerProduct $object
+     *
+     * @return array|\ArrayObject|bool|float|int|string|null
+     */
+    public function normalize($object, string $format = null, array $context = [])
+    {
+        return [
+            'id' => $object->getId(),
+            'container' => $object->getContainer(),
+            'product' => $object->getProduct(),
+            'quantity' => $object->getQuantity(),
+        ];
+    }
+}
+
