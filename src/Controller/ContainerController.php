2577df27875e1a1d9d47d1e34736c1dae0f68b13
Barusa <hugo.famechon@etu.univ-amu.fr>
2021-01-10 15:34:52 +0100

    Normalizer et controller de container

--- src/Controller/ContainerController.php
+++ src/Controller/ContainerController.php
@@ -5,28 +5,22 @@ namespace App\Controller;
 
 use App\Entity\Container;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 // ...
 
 class ContainerController extends AbstractController
 {
 
-    public function GetContainer(int $id): Response
+    public function GetContainer(int $id): JsonResponse
     {
-        $container = $this->getDoctrine()
-            ->getRepository(Container::class)
-            ->find($id);
+        $container = $this->getDoctrine()->getRepository(Container::class)->find($id);
+        return $this->json([$container]);
+    }
 
-        if (!$container) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }
-
-        return new Response('Here are the characteristics of the container number '.$id." : ".$container->getColor());
-
-        // or render a template
-        // in the template, print things with {{ product.name }}
-        // return $this->render('product/show.html.twig', ['product' => $product]);
+    public function ListContainers(): JsonResponse
+    {
+        $container = $this->getDoctrine()->getRepository(Container::class)->findAll();
+        return $this->json([$container]);
     }
 }
